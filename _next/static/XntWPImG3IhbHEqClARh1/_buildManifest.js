533395227027c8ce61154d7f80eb0bd2f849d7f9
OMikkel <57924435+OMikkel@users.noreply.github.com>
2022-09-01 22:04:22 +0000

    Deploying to gh-pages from @ OMikkel/nextjs-omikkel-boilerplate@643159f372dd12d658ef99483cb2d7b46f3dbc46 🚀

---
+++ _next/static/XntWPImG3IhbHEqClARh1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/776-a80911d4b12081d5.js","static/chunks/pages/index-e499cf4c4a74dd70.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
